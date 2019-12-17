7a94a08ccda974e90b690dfd47217248920a8da0
Jacob <jhbriden98@gmail.com>
2019-12-17 14:43:35 +0000

    revisions

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   resources :teachers
   resources :courses
   root to: 'courses#index'
+  match 'search', to: 'courses#search', via: :get
+  match 'search', to: 'teachers#search', via: :get
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
